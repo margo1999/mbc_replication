28335c840ab10685d1e4191c0ee6f9cff2bd5abd
jette_oberlaender <jette.oberlaender@rwth-aachen.de>
2022-07-22 14:52:26 +0200

    added some pylint fixes

--- experiments/sequential_dynamics/submission_cluster_simulation.py
+++ experiments/sequential_dynamics/submission_cluster_simulation.py
@@ -1,10 +1,12 @@
+""" TODO
+"""
 import os
 import sys
 import yaml
 import numpy as np
 
-from clock_net import helper 
-import parameters_space as pars 
+from clock_net import helper
+import parameters_space as pars
 
 # get commmand line arguments
 try:
@@ -27,29 +29,29 @@ if not os.path.exists(logpath):
 
 PS = pars.p
 
-PL=helper.parameter_set_list(PS)
+PL = helper.parameter_set_list(PS)
 params = PL[0]
 
-# save parameters.py  
+# save parameters.py
 helper.copy_scripts(PS['data_path'], "parameters_space.py")
 
-## write (temporary) submission script
-N = len(PL)   ## size of this batch
+# write (temporary) submission script
+N = len(PL)   # size of this batch
 
 print("\nNumber of parameter sets: %d\n" % (N))
 
-JOBMAX=1000      ## max number of jobs in one batch    
+JOBMAX = 1000      # max number of jobs in one batch
 
-for batch_id in range(int(np.ceil(1.*N/JOBMAX))):
+for batch_id in range(int(np.ceil(1. * N / JOBMAX))):
 
-    batch_end = min((batch_id+1)*JOBMAX, N)         # id of parameter set corresponding to end of this batch
-    batch_size = batch_end - batch_id*JOBMAX        # size of this batch
-    submission_script="%s_%d.sh" % (params['data_path']['parameterspace_label'],batch_id)
+    batch_end = min((batch_id + 1) * JOBMAX, N)         # id of parameter set corresponding to end of this batch
+    batch_size = batch_end - batch_id * JOBMAX        # size of this batch
+    submission_script = "%s_%d.sh" % (params['data_path']['parameterspace_label'], batch_id)
 
     file = open(submission_script, 'w')
     file.write('#!/bin/bash\n')
     file.write('#SBATCH --job-name ' + params['data_path']['parameterspace_label'] + '\n')    # set the name of the job
-    file.write('#SBATCH --array 0-%d\n' % (batch_size-1))                                     # launch an array of jobs
+    file.write('#SBATCH --array 0-%d\n' % (batch_size - 1))                                     # launch an array of jobs
     file.write('#SBATCH --time 15:00:00\n')                                                   # specify a time limit
     file.write('#SBATCH --ntasks 1\n')
     file.write('#SBATCH --cpus-per-task %d\n' % params['n_threads'])
@@ -58,14 +60,14 @@ for batch_id in range(int(np.ceil(1.*N/JOBMAX))):
     file.write('#SBATCH --mail-type=BEGIN,END,FAIL,REQUEUE\n')              # send email notifications
     file.write('#SBATCH --mail-user=%s\n' % email)
     file.write('#SBATCH --partition=blaustein,hambach\n')
-    #file.write('source activate clock_network\n')                             # activate conda environment
+    # file.write('source activate clock_network\n')                             # activate conda environment
     file.write('#SBATCH --mem=10000\n')                                      # and reserve 6GB of memory
-    file.write('srun python %s %d $SLURM_ARRAY_TASK_ID %d \n' % (simulation_script,batch_id,JOBMAX) ) # call simulation script
+    file.write('srun python %s %d $SLURM_ARRAY_TASK_ID %d \n' % (simulation_script, batch_id, JOBMAX))  # call simulation script
     file.write('scontrol show jobid ${SLURM_JOBID} -dd # Job summary at exit')
     file.close()
-    
-    ## execute submission_script
+
+    # execute submission_script
     print("submitting %s" % (submission_script))
     os.environ['MKL_SERVICE_FORCE_INTEL'] = '1'
     os.system("sbatch ./%s" % submission_script)
-    #os.system("rm %s" % submission_script)
+    # os.system("rm %s" % submission_script)
--- experiments/sequential_dynamics/training.py
+++ experiments/sequential_dynamics/training.py
@@ -1,91 +1,97 @@
 """
-This script instantiates the Clock Network model, creates, connects, and simulates the network 
+This script instantiates the Clock Network model, creates, connects, and simulates the network TODO
 
 Authors
 ~~~~~~~
 Jette Oberlaender, Younes Bouhadjar
 """
 
-import nest
+import os
 import sys
 import time
-import numpy as np
-import os
 from pprint import pprint
 
-from clock_net import model, helper
+import nest
+import numpy as np
+from clock_net import helper, model
+
 
 def generate_reference_data():
+    """_summary_ TODO documentation
 
-    # ###########################################################
+    Returns:
+        _type_: _description_
+    """
+
+    # ===========================================================
     # import nestml module
     # ===========================================================
-    #nest.Install('../../module/nestml_active_dend_module')
+    # nest.Install('../../module/nestml_active_dend_module')
 
-    #############################################################
-    # get network and training parameters 
+    # ===========================================================
+    # get network and training parameters
     # ===========================================================
     PS = model.get_parameters()
 
     # parameter-set id from command line (submission script)
-    PL = helper.parameter_set_list(PS) 
-   
-    #TODO: use argparse with default values
-    try: 
-        batch_id=int(sys.argv[1])
-        batch_array_id=int(sys.argv[2])
-        JOBMAX=int(sys.argv[3])
-        array_id=batch_id*JOBMAX+batch_array_id
+    PL = helper.parameter_set_list(PS)
+
+    # TODO: use argparse with default values
+    try:
+        batch_id = int(sys.argv[1])
+        batch_array_id = int(sys.argv[2])
+        JOBMAX = int(sys.argv[3])
+        array_id = batch_id * JOBMAX + batch_array_id
     except:
         array_id = 0
 
     params = PL[array_id]
     resultpath = helper.get_data_path(params['data_path'], params['label'])
 
-    # start time 
+    # start time
     time_start = time.time()
 
-    # ###############################################################
+    # ===========================================================
     # specify sequences
-    # ===============================================================
+    # ===========================================================
     sequences, _, vocabulary = helper.generate_sequences(params['task'], params['data_path'], params['label'])
 
-    # ###############################################################
+    # ===========================================================
     # create network
-    # ===============================================================
+    # ===========================================================
     model_instance = model.Model(params, sequences, vocabulary)
     time_model = time.time()
 
     model_instance.create()
     time_create = time.time()
 
-    # ###############################################################
+    # ===========================================================
     # save params to txt file
-    # ===============================================================
+    # ===========================================================
     parameterspacepath = os.path.join(resultpath, 'parameter_space.txt')
     with open(parameterspacepath, 'wt') as file:
         pprint(params, stream=file)
         file.close()
 
-    # TODO: Add time measurement 
+    # TODO: Add time measurement
 
-    # ###############################################################
+    # ===========================================================
     # connect the network
-    # ===============================================================
+    # ===========================================================
     model_instance.connect()
     time_connect = time.time()
-    
+
     # store connections before learning
     if params['store_connections']:
         model_instance.save_connections(synapse_model=params['syn_dict_ee']['synapse_model'], fname='ee_connections_before')
         if params['syn_dict_ei']['synapse_model'] != 'static_synapse':
-            model_instance.save_connections(synapse_model=params['syn_dict_ei']['synapse_model'], fname='ei_connections_before') # TODO: better ask for source and target
-        model_instance.save_connections(fname='all_connections_before')    
+            model_instance.save_connections(synapse_model=params['syn_dict_ei']['synapse_model'], fname='ei_connections_before')  # TODO: better ask for source and target
+        model_instance.save_connections(fname='all_connections_before')
     time_store_connection_before = time.time()
 
-    # ###############################################################
+    # ===========================================================
     # simulate the network
-    # ===============================================================
+    # ===========================================================
     model_instance.simulate()
     time_simulate = time.time()
 
@@ -93,59 +99,56 @@ def generate_reference_data():
     if params['store_connections']:
         model_instance.save_connections(synapse_model=params['syn_dict_ee']['synapse_model'], fname='ee_connections')
         if params['syn_dict_ei']['synapse_model'] != 'static_synapse':
-            model_instance.save_connections(synapse_model=params['syn_dict_ei']['synapse_model'], fname='ei_connections') # TODO: better ask for source and target
+            model_instance.save_connections(synapse_model=params['syn_dict_ei']['synapse_model'], fname='ei_connections')  # TODO: better ask for source and target
         model_instance.save_connections(fname='all_connections')
     time_store_connection_after = time.time()
 
     def print_times(file=sys.stdout):
 
         print(
-        '\nTimes of Rank {}:\n'.format(
-            nest.Rank()) +
-        '  Total time:                 {:.3f} s\n'.format(
-            time_store_connection_after -
-            time_start) +
-        '  Time to initialize:         {:.3f} s\n'.format(
-            time_model -
-            time_start) +
-        '  Time to create:             {:.3f} s\n'.format(
-            time_create -
-            time_model) +
-        '  Time to connect:            {:.3f} s\n'.format(
-            time_connect -
-            time_create) +
-        '  Time to store connections:  {:.3f} s\n'.format(
-            time_store_connection_before -
-            time_connect) +    
-        '  Time to simulate:           {:.3f} s\n'.format(
-            time_simulate -
-            time_store_connection_before) +
-        '  Time to store connections:  {:.3f} s\n'.format(
-            time_store_connection_after -
-            time_simulate),
-            
+            '\nTimes of Rank {}:\n'.format(
+                nest.Rank()) +
+            '  Total time:                 {:.3f} s\n'.format(
+                time_store_connection_after -
+                time_start) +
+            '  Time to initialize:         {:.3f} s\n'.format(
+                time_model -
+                time_start) +
+            '  Time to create:             {:.3f} s\n'.format(
+                time_create -
+                time_model) +
+            '  Time to connect:            {:.3f} s\n'.format(
+                time_connect -
+                time_create) +
+            '  Time to store connections:  {:.3f} s\n'.format(
+                time_store_connection_before -
+                time_connect) +
+            '  Time to simulate:           {:.3f} s\n'.format(
+                time_simulate -
+                time_store_connection_before) +
+            '  Time to store connections:  {:.3f} s\n'.format(
+                time_store_connection_after -
+                time_simulate),
+
             file=file)
-    
+
     print_times()
 
-    # ###############################################################
+    # ===========================================================
     # save times to txt file
-    # ===============================================================
+    # ===========================================================
     timepath = os.path.join(resultpath, 'simulation_times.txt')
     print_times(file=open(timepath, 'a'))
 
-
     return resultpath
 
-if __name__ == '__main__':    
-    from figures import plot_results
-    import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
     import matplotlib
+    import matplotlib.pyplot as plt
+    from figures import plot_results
     matplotlib.use("Agg")
 
     resultpath = generate_reference_data()
-    plot_results.plot_results(os.path.join(resultpath,f"simulation_finished"))
+    plot_results.plot_results(os.path.join(resultpath, "simulation_finished"))
     plt.show()
-
-    
-    
