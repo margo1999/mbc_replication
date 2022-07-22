c90853ad59edddbd404825cb577caba540c6ea9c
jette_oberlaender <jette.oberlaender@rwth-aachen.de>
2022-07-22 16:15:07 +0200

    added comment on missing documentation

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
+""" TODO
+"""
 from setuptools import setup
 
 setup(
-     name="clock_network", 
+     name="clock_network",
      version="1.0",
      description="PyNEST implementation of the clock network proposed by Maes et al. 2020",
-      
      author="Jette Oberlaender, Younes Bouhadjar",
      author_email="y.bouhadjar@fz-juelich.de",
      url='https://github.com/INM-6/clock_network',
-
      packages=["clock_net"]
      )
