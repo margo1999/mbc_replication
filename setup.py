aebe9d4833d87afa7bc73347ae511ccffb6ff076
jette_oberlaender <jette.oberlaender@rwth-aachen.de>
2022-08-04 16:56:20 +0200

    added more documentation for setup

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-""" TODO
 """
-from setuptools import setup
+Installs the "mbc_network" library.
+"""
+from setuptools import setup, find_packages
 
 setup(
-     name="clock_network",
-     version="1.0",
-     description="PyNEST implementation of the clock network proposed by Maes et al. 2020",
-     author="Jette Oberlaender, Younes Bouhadjar",
-     author_email="y.bouhadjar@fz-juelich.de",
-     url='https://github.com/INM-6/clock_network',
-     packages=["clock_net"]
-     )
+    name="mbc_network",
+    version="1.0",
+    description="PyNEST implementation of the network proposed by Maes et al. 2020",
+    author="Jette Oberlaender, Younes Bouhadjar",
+    author_email="y.bouhadjar@fz-juelich.de",
+    url='https://github.com/margo1999/mbc_replication',
+    packages=find_packages()
+)
